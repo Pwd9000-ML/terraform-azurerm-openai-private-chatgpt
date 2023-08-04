dd9fd3204a82fbe2bcaa2b0c7227a68363b5ab53
Pwd9000-ML <pwd9000@hotmail.co.uk>
2023-08-04 17:53:59 +0100

    update module test

--- main.tf
+++ main.tf
@@ -9,15 +9,22 @@
 module "create_openai_service" {
   source = "./modules/openai_service"
   # Only deploy a new openai service 'var.create_openai_service' is true
-  count                      = var.create_openai_service == true ? 1 : 0
-  openai_resource_group_name = var.openai_resource_group_name
-  location                   = var.location
-  #   #virtual_network_name        = var.virtual_network_name
-  #   #vnet_address_space          = var.vnet_address_space
-  #   #subnet_config               = var.subnet_config
-  #   #subnet_config_delegated_aci = var.subnet_config_delegated_aci
-  #   #private_dns_zones           = var.private_dns_zones
-  tags = var.tags
+  count                              = var.create_openai_service == true ? 1 : 0
+  resource_group_name                = var.openai_resource_group_name
+  location                           = var.location
+  account_name                       = var.openai_account_name
+  sku_name                           = var.openai_sku_name
+  custom_subdomain_name              = var.openai_custom_subdomain_name
+  dynamic_throttling_enabled         = var.openai_dynamic_throttling_enabled
+  fqdns                              = var.openai_fqdns
+  local_auth_enabled                 = var.openai_local_auth_enabled
+  outbound_network_access_restricted = var.openai_outbound_network_access_restricted
+  public_network_access_enabled      = var.openai_public_network_access_enabled
+  customer_managed_key               = var.openai_customer_managed_key
+  identity                           = var.openai_identity
+  network_acls                       = var.openai_network_acls
+  storage                            = var.openai_storage
+  tags                               = var.tags
 }
 
 # module "create_model_deployment" {
--- variables.tf
+++ variables.tf
@@ -33,5 +33,122 @@ variable "openai_resource_group_name" {
 }
 
 ##########################################
-# Networking                             #
+# OpenAI Service                         #
 ##########################################
+variable "create_openai_service" {
+  type        = bool
+  description = "Create the OpenAI service."
+  default     = true
+}
+
+variable "openai_account_name" {
+  type        = string
+  description = "Name of the OpenAI service."
+  default     = "demo-account"
+}
+
+variable "openai_sku_name" {
+  type        = string
+  description = "SKU name of the OpenAI service."
+  default     = "S0"
+}
+
+variable "openai_custom_subdomain_name" {
+  type        = string
+  description = "The subdomain name used for token-based authentication. Changing this forces a new resource to be created."
+  default     = null
+}
+
+variable "openai_dynamic_throttling_enabled" {
+  type        = bool
+  description = "Determines whether or not dynamic throttling is enabled. If set to `true`, dynamic throttling will be enabled. If set to `false`, dynamic throttling will not be enabled."
+  default     = null
+}
+
+variable "openai_fqdns" {
+  type        = list(string)
+  description = "List of FQDNs allowed for the Cognitive Account."
+  default     = null
+}
+
+variable "openai_local_auth_enabled" {
+  type        = bool
+  description = "Whether local authentication methods is enabled for the Cognitive Account. Defaults to `true`."
+  default     = true
+}
+
+variable "openai_outbound_network_access_restricted" {
+  type        = bool
+  description = "Whether or not outbound network access is restricted."
+  default     = false
+}
+
+variable "openai_public_network_access_enabled" {
+  type        = bool
+  description = "Whether or not public network access is enabled for the Cognitive Account."
+  default     = true
+}
+variable "openai_customer_managed_key" {
+  type = object({
+    key_vault_key_id   = string
+    identity_client_id = optional(string)
+  })
+  default     = null
+  description = <<-DESCRIPTION
+    type = object({
+      key_vault_key_id   = (Required) The ID of the Key Vault Key which should be used to Encrypt the data in this OpenAI Account.
+      identity_client_id = (Optional) The Client ID of the User Assigned Identity that has access to the key. This property only needs to be specified when there're multiple identities attached to the OpenAI Account.
+    })
+  DESCRIPTION
+}
+
+variable "openai_identity" {
+  type = object({
+    type         = string
+    identity_ids = optional(list(string))
+  })
+  default     = null
+  description = <<-DESCRIPTION
+    type = object({
+      type         = (Required) The type of the Identity. Possible values are `SystemAssigned`, `UserAssigned`, `SystemAssigned, UserAssigned`.
+      identity_ids = (Optional) Specifies a list of User Assigned Managed Identity IDs to be assigned to this OpenAI Account.
+    })
+  DESCRIPTION
+}
+
+variable "openai_network_acls" {
+  type = set(object({
+    default_action = string
+    ip_rules       = optional(set(string))
+    virtual_network_rules = optional(set(object({
+      subnet_id                            = string
+      ignore_missing_vnet_service_endpoint = optional(bool, false)
+    })))
+  }))
+  default     = null
+  description = <<-DESCRIPTION
+    type = set(object({
+      default_action = (Required) The Default Action to use when no rules match from ip_rules / virtual_network_rules. Possible values are `Allow` and `Deny`.
+      ip_rules       = (Optional) One or more IP Addresses, or CIDR Blocks which should be able to access the Cognitive Account.
+      virtual_network_rules = optional(set(object({
+        subnet_id                            = (Required) The ID of a Subnet which should be able to access the OpenAI Account.
+        ignore_missing_vnet_service_endpoint = (Optional) Whether ignore missing vnet service endpoint or not. Default to `false`.
+      })))
+    }))
+  DESCRIPTION
+}
+
+variable "openai_storage" {
+  type = list(object({
+    storage_account_id = string
+    identity_client_id = optional(string)
+  }))
+  default     = []
+  description = <<-DESCRIPTION
+    type = list(object({
+      storage_account_id = (Required) Full resource id of a Microsoft.Storage resource.
+      identity_client_id = (Optional) The client ID of the managed identity associated with the storage resource.
+    }))
+  DESCRIPTION
+  nullable    = false
+}
