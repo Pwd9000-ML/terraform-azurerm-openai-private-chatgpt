aed73ef9bbdb0c8a4af5038a3d2f0d0feb63feb8
Pwd9000-ML <pwd9000@hotmail.co.uk>
2024-01-17 03:16:44 +0000

    test

--- tests/auto_test1/data.tf
+++ tests/auto_test1/data.tf
@@ -1,9 +1,9 @@
-data "azurerm_subnet" "subnet" {
-  for_each             = var.subnet_config
-  name                 = var.subnet_config.subnet_name
-  virtual_network_name = var.virtual_network_name
-  resource_group_name  = var.resource_group_name
-}
+#data "azurerm_subnet" "subnet" {
+#  for_each             = var.subnet_config
+#  name                 = var.subnet_config.subnet_name
+#  virtual_network_name = var.virtual_network_name
+#  resource_group_name  = var.resource_group_name
+#}
 
 # data "azurerm_key_vault" "gpt" {
 #   name                = local.kv_config.name
