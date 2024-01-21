c4b1e8688a9704e8343f7a41196760e27a344c7a
Pwd9000-ML <pwd9000@hotmail.co.uk>
2024-01-21 21:30:36 +0000

    test

--- 06_librechat_app_config.tf
+++ 06_librechat_app_config.tf
@@ -7,16 +7,14 @@ locals {
     DOCKER_ENABLE_CI                    = false
     WEBSITES_PORT                       = var.libre_app_port
     PORT                                = var.libre_app_port
-    WEBSITES_CONTAINER_START_TIME_LIMIT = 2200
-    DOCKER_CUSTOM_IMAGE_NAME            = "ghcr.io/danny-avila/librechat-dev-api:latest"
+    WEBSITES_CONTAINER_START_TIME_LIMIT = 1500
+    DOCKER_CUSTOM_IMAGE_NAME            = "ghcr.io/danny-avila/librechat-dev-api:d7b4ed3079e357e316e2398093acc456f128dc6e"
     NODE_ENV                            = "production" #######
-    USE_REDIS                           = false
 
     ### Server Configuration ###
     APP_TITLE     = var.libre_app_title
     CUSTOM_FOOTER = var.libre_app_custom_footer
     HOST          = var.libre_app_host
-    # PORT          = var.libre_app_port
     MONGO_URI     = var.libre_app_mongo_uri != null ? var.libre_app_mongo_uri : "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.openai_cosmos_uri.id})"
     DOMAIN_CLIENT = var.libre_app_domain_client
     DOMAIN_SERVER = var.libre_app_domain_server
